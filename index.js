d1b27b35cf98968c794c4ae02bd64e412266bb7d
Chandelier Ephrata <chandephrata@gmail.com>
2022-03-17 09:32:17 -0600

    commit tree 1e201df1703abba83c84da03dd24edf1948de545

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   });
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+  //document.addEventListener( "DOMContentLoaded", function(){
+
+    document.addEventListener("DOMContentLoaded", function (e) {
+       
+        const text = document.querySelector("#text");
+        console.log(text.innerHTML);
+        text.innerHTML = "This is really cool!";
+      });
+      console.log(
+        
+      );
